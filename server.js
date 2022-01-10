d6d900cf1fdd0499926fca2e618fddcc0e9a076c
Darwin Smith <darwin911@gmail.com>
2022-01-10 15:31:51 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,42 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const port = process.env.PORT || 5000;
+const puppeteer = require("puppeteer");
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: `Server is running on port: ${port}` });
+});
+
+app.post("/news", async (req, res) => {
+  const newsUrl = req.body.newsUrl;
+
+  if (newsUrl) {
+    const browser = await puppeteer.launch({ headless: true, devtools: true });
+    const page = await browser.newPage();
+    await page.goto(newsUrl, { waitUntil: "networkidle2" });
+    const title = await page.evaluate(() =>
+      Array.from(document.getElementsByTagName("h1")).map(
+        (h1) => h1.textContent
+      )
+    );
+    const paragraphs = await page.evaluate(() =>
+      Array.from(document.getElementsByTagName("p")).map(
+        (item) => item.textContent
+      )
+    );
+
+    return res.json({ title, paragraphs });
+  } else {
+    return res.sendStatus(500);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
